ad9a3bbe5da90d838ed6be63d575d2060f0843f0
utkarshtiwari <tiwari.utkarsh2611@gmail.com>
2023-02-15 22:31:46 +0530

    Store Added

---
+++ javascript/Data.js
@@ -0,0 +1,26 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "D phone 14",
+    price: 70000,
+    img: "./../img/D11.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "DPhone 13",
+    price: 90999,
+    img: "./../img/D13.jpeg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "DPhone 12",
+    price: 130000,
+    img: "./../img/D12.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Dphone 11",
+    price: 133000,
+    img: "./../img/D11.jpg",
+  },
+];
