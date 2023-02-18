a896e599ac0301b65713c8e5a5fb288040b098d5
Sumit Kulhari <sk4213@srmist.edu.in>
2023-02-18 15:04:33 +0530

    Updated UserRegisteration Page

---
+++ javascript/user.js
@@ -0,0 +1,52 @@
+/**
+ * Helper function for POSTing data as JSON with fetch.
+ *
+ * @param {Object} options
+ * @param {string} options.url - URL to POST data to
+ * @param {FormData} options.formData - `FormData` instance
+ * @return {Object} - Response body from URL that was POSTed to
+ */
+const registerForm = document.getElementById("register");
+
+async function postFormDataAsJson({ url, formData }) {
+    const plainFormData = Object.fromEntries(formData.entries());
+    const formDataJsonString = JSON.stringify(plainFormData);
+
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: formDataJsonString,
+    };
+
+    const response = await fetch(url, fetchOptions);
+
+    if (!response.ok) {
+        const errorMessage = await response.text();
+        throw new Error(errorMessage);
+    }
+
+    return response.json();
+}
+
+async function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const url = form.action;
+
+    try {
+        const formData = new FormData(form);
+        const responseData = await postFormDataAsJson({ url, formData });
+        window.location = "https://www.tutorialspoint.com";
+
+        console.log({ responseData });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+registerForm.addEventListener("submit", handleFormSubmit);
+window.location = "https://www.tutorialspoint.com";
