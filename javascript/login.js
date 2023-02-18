8b2601bd1a79af3ddd625d0f56c02b9058d3e96e
Sumit Kulhari <sk4213@srmist.edu.in>
2023-02-18 14:56:09 +0530

    Updated Login Page

---
+++ javascript/login.js
@@ -0,0 +1,58 @@
+
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const loginForm = document.getElementById('login-form');
+
+async function postFormDataAsJson({ url, formData }) {
+    const plainFormData = Object.fromEntries(formData.entries());
+    const formDataJsonString = JSON.stringify(plainFormData);
+
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: formDataJsonString,
+    };
+
+    const response = await fetch(url, fetchOptions);
+
+    if (!response.ok) {
+        const errorMessage = await response.text();
+        throw new Error(errorMessage);
+    }
+
+    return response.json();
+}
+
+/**
+ * Event handler for a form submit event.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event
+ *
+ * @param {SubmitEvent} event
+ */
+async function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const url = form.action;
+
+    try {
+        const formData = new FormData(form);
+        const responseData = await postFormDataAsJson({ url, formData });
+
+        console.log({ responseData });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// const exampleForm = document.getElementById("example-form");
+loginForm.addEventListener("submit", handleFormSubmit);
+//
+// if (loginSuccessful) {
+//     // Replace "home.html" with the URL of the page you want to redirect to
+//     window.location.href = "home.html";
+// }
